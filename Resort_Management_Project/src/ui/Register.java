367043ad3698c11c7db7b7c27ae78d48d5a1e005
Thikkavarapu Manikanta Reddy <t.manikantareddy160@gmail.com>
2022-12-03 18:29:11 -0500

    Role Data Changes

--- Resort_Management_Project/src/ui/Register.java
+++ Resort_Management_Project/src/ui/Register.java
@@ -69,7 +69,7 @@ public class Register extends javax.swing.JPanel {
             }
         });
 
-        cmbRole.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Customer", "Admin", "Vehicle Contractor", "Pool Incharge", "Gaming Incharge", "Janitor", "" }));
+        cmbRole.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Customer", "Admin", "Vehicle Contractor", "Pool Incharge", "Gaming Incharge", "Janitor" }));
         cmbRole.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 cmbRoleActionPerformed(evt);
