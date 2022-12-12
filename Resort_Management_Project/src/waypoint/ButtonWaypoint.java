f2a5d35c195bc820eb9352345285876187aff9a9
Thikkavarapu Manikanta Reddy <t.manikantareddy160@gmail.com>
2022-12-11 22:05:43 -0500

    Changes

--- Resort_Management_Project/src/waypoint/ButtonWaypoint.java
+++ Resort_Management_Project/src/waypoint/ButtonWaypoint.java
@@ -9,7 +9,7 @@ public class ButtonWaypoint extends JButton {
 
     public ButtonWaypoint() {
         setContentAreaFilled(false);
-        setIcon(new ImageIcon(getClass().getResource("/icon/pin.png")));
+        setIcon(new ImageIcon(getClass().getResource("/images/pin.png")));
         setCursor(new Cursor(Cursor.HAND_CURSOR));
         setSize(new Dimension(24, 24));
     }
